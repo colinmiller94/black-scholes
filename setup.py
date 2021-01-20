e463c296fd282f110112ae197f0cd9c6fae1dbf8
Colin <colinmiller.94@gmail.com>
2021-01-20 14:44:54 -0500

    edit setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 
 setuptools.setup(
-    name="Option_Scraper",
-    version="0.0.1",
-    author="Colin Miller",
-    author_email="colinmiller.94@gmail.com",
+    name='black-scholes',
+    version='0.0.1',
+    author='Colin Miller',
+    author_email='colinmiller.94@gmail.com',
     license='Proprietary',
-    description="A personal project",
-    long_description_content_type="text/markdown",
-    url="https://github.com/colinmiller94/OptionScraper",
+    description='A personal project',
+    long_description_content_type='text/markdown',
+    url='https://github.com/colinmiller94/black-scholes',
     packages=['tests', 'scripts'],
     include_package_data=True,
     zip_safe=False
